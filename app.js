2b927e83209f86b539143854f8f20cf01d1a03b4
徐同保 <xutongbao@vackbot.com>
2019-07-13 15:10:19 +0800

    redis

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const JSEncrypt = require('node-jsencrypt');
 const svgCaptcha = require('svg-captcha')
+const redis = require('redis')
 const { find, registerCheckUsername, register, list, deleteItem, addItem } = require('./utils')
 const app = express()
 var bodyParser = require('body-parser');
+
+const client = redis.createClient();
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //token仓库
 let tokenHistory = []
-let captchaIdHistory = []
+let captchaIdHistory = {}
 
 //允许跨域
 app.use('*', (req, res, next) => {
@@ -61,21 +68,23 @@ setInterval(() => {
   }
 }, 10000)
 
-
 //登陆
 app.post('/login', async function (req, res) {
-  let { username, password, captcha, captchaId } = req.body
-  let serverCaptcha
-  for (let i = 0; i < captchaIdHistory.length; i++) {
-    if (captchaId === captchaIdHistory[i].captchaId) {
-      serverCaptcha = captchaIdHistory[i].text
-    }
-  }
-  if (captcha && captchaId && captcha === serverCaptcha) {
+  let { username, password, captcha } = req.body
+
+  let token = req.headers['token']
+
+  let redisCaptcha = await new Promise( (resolve) => {
+    client.get(token,function(err, res){
+      return resolve(res);
+    });
+  });    
+  console.log('login', redisCaptcha)
+  if (captcha && captcha === redisCaptcha) {
   } else {
     res.send({
       code: 400,
-      message: '验证码错误'
+      message: '验证码错误或过期'
     })
     return    
   }
@@ -83,7 +92,6 @@ app.post('/login', async function (req, res) {
   let decrypt = new JSEncrypt()
   decrypt.setPrivateKey('MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCBlsrfdbHJOb6ZCWeukOGuVGVIyLnjNJUpQgK+HbfgN83n9Q65F3nFwhDXvNIZf+m21xkz3mlMm/UwcOPgW+MS1YDJfetMv/q0aLHJVma/lP3eiTpld7pkPIkhDmZutAUGHb+Kb3aA4j6IYh5tvQWKERgKqvULTyKT+mnGK2vr4OjtscvUtl5h1gBD+GChj0gjO3Y3lAGUU6Zowr32iZIhDda7qiN82VroQ1Z2ZbfLHDsXSwNZA43R2xhA6JTDwNDxRXkdYF//BuGlNYcqt9YCTeTNmEPUAUGG2fCRAq+XQdAuXeQ/9uMEOrShwWwK2iS9DHKsHVzQk6qXL17yHNT9AgMBAAECggEAT1IwvdU5zLUW0G/sUfMG4QFxubp75fICt21bZmmkao9d7vSWXX9cH0tpqdQ/OjKMqUlCePhTtfvkehEvut4jwRyEp1D3HVSJrbJD2LyVSgF136hZ4VAG/GH9gglhc7OnO5Yr7x3JCJzzr+F3RK6l3FwYPjdkpNZ8kN1/rzhW59syV5SSfZxhqS8j454tow6vdhjIHhsuZj/JR1yo6zp+Jgh1m/Js2tPq/3VtbhUsyWLbkUxZ0hFtlAIDhYYyu//9eHthEgelgXePXuPXTLb234PjTFs5gh17qiMozVJGuJLdOuw5SPggXkOwoM05yNyk1VcYVnA6PLh9uqy6J/vg4QKBgQDWuJh+qoFOniUH7DIob0LootQQZX8JEPu1opn308Ee3MurWImD8iB6Sgv4v1p5cggxz180uINf8CgfnKnRFY7iZE4rrHDhpepnJjc3EoFuBr4qOkKxt4+UpfpW+akingL6e2+TsE3hiWn7+anQ2CDHBtewb1zPmcEUCoXhGhwiyQKBgQCagHXXYKZkQP8ONt68d613cmnsarJ+A7WTPLsT9FdR/ue5f2XfLZieiJrz1XE3gNT0M4uGwQ1K5xGVDxAlDowICTR62bLTfcrlgDEabX7Ie6LoKHn94DuzYJp0hGIOoW/B/MUm1jGnabmRwd5fMN7kYkTHW76GOgCdtworFpzmlQKBgAKWmw/70i82bM/ZowklvjK7s7622n8rez1HRn/cR5mto67fFFlI0+dleRt4gwivvTWMgGNMThkb9f/bjZh9oAlOmDqii4HM1DSGG2hIitLetfAgX6lwwwRRs+DUhZtrODH4xvGK0IWFeIeXDNr9n/eGN5di4EWIXvF05wQ9olUxAoGAGFOnulzWdiyhvEw5b/VsE8WpFfeK2TqeSWujjTXI1flL5TJBCQ313OhndYMjC3k2jqjbDEGzZRrS8uRnR15uAAnrAHjspk5FNzOjB6U79Dc7ZFCTPL0kyU6pozUA94rFvQaeyEmrkc+cz212JQjd35n1eGpiqmHRnQOoDzbFR60CgYBYdZj0hYgVXVeSgZVWQGteLi46s/puRLgLZLjudqUZcQIq4GwBpVfSbsfuy+HjBc1GhlYpMp6ZZPzvPKasYyyFy2lgJj0AT7INuA9bnILYvqMzeCVL8RrEFrIGff64mxIMN/qE9uu+e9x2nWYdfXPfjaL6r7eOaZ59kE5UsCg/gA==')
   password = decrypt.decrypt(password)
-  console.log(password)
   const data = await find(username)
   if (!data.password) {
     res.send({
@@ -94,7 +102,6 @@ app.post('/login', async function (req, res) {
   }
   let dbPassword = data.password.replace(/%2B/g, '+')
   dbPassword = decrypt.decrypt(dbPassword)
-  console.log(dbPassword)
   if (password === dbPassword) {
     let token = getID(10)
 
@@ -112,7 +119,6 @@ app.post('/login', async function (req, res) {
       },
       message: '登陆成功'
     })
-    console.log('登陆成功')
   } else {
     res.send({
       code: 400,
@@ -130,7 +136,11 @@ app.get('/captcha', function (req, res) {
     captcha: captcha.data,
     text: text
   }
-  captchaIdHistory.push(temp)
+  captchaIdHistory[captchaId] = text
+  client.set(captchaId, text, 'EX', 60)  //60秒后验证码过期知道
+  client.get(captchaId, function (err,v) {
+      console.log("图形验证码的值存入redis，值为：",v);
+  })  
   res.send({
     code: 200,
     data: temp,
